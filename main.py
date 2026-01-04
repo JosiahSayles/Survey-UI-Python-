a5ae7b1c2b91b62434d67d9d09cc231bf4c0fe66
Josiah Sayles <Josiah.Sayles@gmail.com>
2026-01-03 17:06:41 -0700

    Added survey question box frame and start creating labels and input boxes for questions 1-3, also started created 5 star rating system for survey. Added Imgs folder to hold empystar png and shining star png.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import tkinter
-from tkinter import ttk
-from tkinter import messagebox
+import tkinter 
+
+
 
 
 window = tkinter.Tk()
 window.title("Survey form")
+
+window.columnconfigure(0, weight=1)
+window.rowconfigure(0, weight=1)
+window.rowconfigure(1, weight=1)
 
 frame = tkinter.Frame(window)
 frame.pack()
@@ -38,6 +42,28 @@ for widget in survey_user_info_frame.winfo_children():
 
 #Survey questions 
 
+survey_questions_frame = tkinter.LabelFrame(frame)
+survey_questions_frame.grid(row=1,column=0, sticky= "news", padx=10, pady=25)
 
+question_1_label = tkinter.Label(survey_questions_frame, text="Question 1. Please let us know how your experience was with our company?")
+question_1_label.grid(row=0, column=0)
+question_1_input = tkinter.Text(survey_questions_frame, height=5, width=30, bd=2)
+question_1_input.grid(row=1,column=0, sticky="news")
+
+question_2_label = tkinter.Label(survey_questions_frame, text="Question 2. What could we have changed to make your experience better?")
+question_2_label.grid(row=2,column=0)
+question_2_input = tkinter.Text(survey_questions_frame, height=5, width=30, bd=2)
+question_2_input.grid(row=3, column=0, sticky="news" )
+
+question_3_label = tkinter.Label(survey_questions_frame, text="Question 3. What additional or  general comments would you like to add?")
+question_3_label.grid(row=4, column=0)
+question_3_input = tkinter.Text(survey_questions_frame, height=5, width=30, bd=2)
+question_3_input.grid(row=5, column=0, sticky="news") 
+
+question_4_label = tkinter.Label(survey_questions_frame, text="Question 4. On a Scale of 1 to 5 how would you rate your overall service? ")
+question_4_label.grid(row=6, column=0)
+# question_4_star_rating 
+for widget in survey_questions_frame.winfo_children():
+    widget.grid_configure(padx=20, pady=10)
 
 window.mainloop()
